2c4a188e4cdd8f6d57442e2cd16214e76fcc2ddf
Amrit Rai <iamamrit27@gmail.com>
2024-05-22 12:14:43 +0530

    setup

---
+++ src/variables.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let name = "Amrit";
+    let age = 20;
+    let height = 5.11;
+
+    println!("Name: {}", name);
+    println!("Age: {}", age);
+    println!("Height: {}", height);
+}

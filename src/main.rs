3e68d7794db0fefb8ec9d6ed4d6b9a673654a595
Amrit Rai <iamamrit27@gmail.com>
2024-06-16 14:22:41 +0530

    learnt heaps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    stack_fn();
+    update_string();
+    heap_fn();
+}
+
+fn stack_fn(){
+    let a: i32 = 10;
+    let b: i32 = 20;
+    let c = a + b; 
+    println!("Stack funtion: The sum of {}, {} is {}", a, b, c);
+}
+
+fn heap_fn(){
+    let s1 = String::from("Hello");
+    let s2 = String::from("World");
+    let combined = format!("{}, {}", s1, s2);
+    println!("Heap function: Combined String is '{}'", combined);
+}
+
+fn update_string(){
+    let mut s = String::from("Initial String");
+    println!("Before update: {}", s);
+    println!("Capacity: {}, Length: {}, Pointer: {:p}", s.capacity(), s.len(), s.as_ptr());
+
+    s.push_str("Additional");
+    println!("Capacity: {}, Length: {}, Pointer: {:p}", s.capacity(), s.len(), s.as_ptr());
+    println!("After update: {}", s);
 }
--- src/variables.rs
+++ src/variables.rs
@@ -1,7 +1,14 @@
 fn main() {
+    let mut x: i8 = 1;
+    
+    for _i in 1..10 {
+        x = x + 1;
+        println!("{}", x);
+    }
+
     let name = "Amrit";
-    let age = 20;
-    let height = 5.11;
+    let age: u8 = 20;
+    let height: f32 = 5.11;
 
     println!("Name: {}", name);
     println!("Age: {}", age);
